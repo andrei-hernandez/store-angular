107684e4cc5fcbdc9545c7eddd3e3c5e78cda861
Andrei Hernandez <andrei.hernandez@contrastsecurity.com>
2024-12-15 00:16:25 -0600

    remove prerender with conflict on SSR

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,16 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'product/:id',
+    renderMode: RenderMode.Prerender,
+    getPrerenderParams: async () => {
+      // Define the parameters for prerendering
+      // This params are prerendered for the route /product/:id
+      // This config means that the route /product/1, /product/2 and /product/3 will be prerendered no matter if the api response is different
+      return [{ id: '1' }, { id: '2' }, { id: '3' }];
+    }
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
